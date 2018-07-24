bbd4c1724f3d12cae8beb38c4255744ba64a46ad
Vilislav Ivanov <vilislav.ivanov91@abv.bg>
2018-07-24 20:29:12 +0300

    export static handler in separate folder

---
+++ nodejs-web-server/handlers/staticHandler.js
@@ -0,0 +1,43 @@
+const fs = require('fs')
+
+const getContentType = pathName => {
+  if (pathName.endsWith('.html')) {
+    return 'text/html'
+  } else if (pathName.endsWith('.js')) {
+    return 'application/json'
+  } else if (pathName.endsWith('.css')) {
+    return 'text/css'
+  } else if (pathName.endsWith('.ico')) {
+    return 'image/x-icon'
+  }
+}
+
+const staticHandler = (req, res) => {
+  if (req.method === 'GET' && req.pathName.startsWith('/public')) {
+    if (req.pathName.endsWith('.html') || req.pathName.endsWith('.js') || req.pathName.endsWith('.css') || req.pathName.endsWith('.ico')) {
+      fs.readFile('.' + req.pathName, (err, data) => {
+        if (err) {
+          res.writeHead(200, {
+            'content-type': 'text/plain'
+          })
+          res.write('error')
+          res.end()
+        } else {
+          res.writeHead(200, {
+            'content-type': getContentType(req.pathName)
+          })
+          res.write(data)
+          res.end()
+        }
+      })
+    } else {
+      res.writeHead(403, {
+        'content-type': 'text/plain'
+      })
+      res.write('Forbidden')
+      res.end()
+    }
+  }
+}
+
+module.exports = staticHandler
--- nodejs-web-server/index.js
+++ nodejs-web-server/index.js
@@ -1,47 +1,11 @@
 const http = require('http')
-const fs = require('fs')
 const url = require('url')
 const port = 4243
-
-const getContentType = pathName => {
-  if (pathName.endsWith('.html')) {
-    return 'text/html'
-  } else if (pathName.endsWith('.js')) {
-    return 'application/json'
-  } else if (pathName.endsWith('.css')) {
-    return 'text/css'
-  } else if (pathName.endsWith('.ico')) {
-    return 'image/x-icon'
-  }
-}
+const staticHandler = require('./handlers/staticHandler')
 
 http.createServer((req, res) => {
-  const pathName = url.parse(req.url).pathname
-  if (req.method === 'GET' && pathName.startsWith('/public')) {
-    if (pathName.endsWith('.html') || pathName.endsWith('.js') || pathName.endsWith('.css') || pathName.endsWith('.ico')) {
-      fs.readFile('.' + pathName, (err, data) => {
-        if (err) {
-          res.writeHead(200, {
-            'content-type': 'text/plain'
-          })
-          res.write('error')
-          res.end()
-        } else {
-          res.writeHead(200, {
-            'content-type': getContentType(pathName)
-          })
-          res.write(data)
-          res.end()
-        }
-      })
-    } else {
-      res.writeHead(403, {
-        'content-type': 'text/plain'
-      })
-      res.write('Forbidden')
-      res.end()
-    }
-  }
+  req.pathName = url.parse(req.url).pathname
+  staticHandler(req, res)
 }).listen(port, () => {
   console.log(`App up and running at localhost:${port}`)
 })
