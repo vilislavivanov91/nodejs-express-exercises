a908656be2516382b02776b207bdfd3b8ab1b3cd
Vilislav Ivanov <vilislav.ivanov91@abv.bg>
2018-07-24 19:59:12 +0300

    Serving static files from public folder

--- nodejs-web-server/index.js
+++ nodejs-web-server/index.js
@@ -1,12 +1,40 @@
 const http = require('http')
-const port = 4242
+const fs = require('fs')
+const url = require('url')
+const port = 4243
+
+const getContentType = pathName => {
+  if (pathName.endsWith('.html')) {
+    return 'text/html'
+  } else if (pathName.endsWith('.js')) {
+    return 'application/json'
+  } else if (pathName.endsWith('.css')) {
+    return 'text/css'
+  } else if (pathName.endsWith('.ico')) {
+    return 'image/x-icon'
+  }
+}
 
 http.createServer((req, res) => {
-  res.writeHead(200, {
-    'content-type': 'text/plain'
-  })
-  res.write('Hello from node')
-  res.end()
+  const pathName = url.parse(req.url).pathname
+  console.log(pathName)
+  if (req.method === 'GET' && pathName.startsWith('/public')) {
+    fs.readFile('.' + pathName, (err, data) => {
+      if (err) {
+        res.writeHead(200, {
+          'content-type': 'text/plain'
+        })
+        res.write('error')
+        res.end()
+      } else {
+        res.writeHead(200, {
+          'content-type': getContentType(pathName)
+        })
+        res.write(data)
+        res.end()
+      }
+    })
+  }
 }).listen(port, () => {
   console.log(`App up and running at localhost:${port}`)
 })
