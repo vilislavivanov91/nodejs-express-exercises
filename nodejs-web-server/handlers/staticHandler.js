1242f14a2215914afa86654949fc011d3a8d7b66
Vilislav Ivanov <vilislav.ivanov91@abv.bg>
2018-07-24 20:32:37 +0300

    export getContentType function in separate folder

--- nodejs-web-server/handlers/staticHandler.js
+++ nodejs-web-server/handlers/staticHandler.js
@@ -1,16 +1,5 @@
 const fs = require('fs')
-
-const getContentType = pathName => {
-  if (pathName.endsWith('.html')) {
-    return 'text/html'
-  } else if (pathName.endsWith('.js')) {
-    return 'application/json'
-  } else if (pathName.endsWith('.css')) {
-    return 'text/css'
-  } else if (pathName.endsWith('.ico')) {
-    return 'image/x-icon'
-  }
-}
+const getContentType = require('../utility/get-content-type')
 
 const staticHandler = (req, res) => {
   if (req.method === 'GET' && req.pathName.startsWith('/public')) {
---
+++ nodejs-web-server/utility/get-content-type.js
@@ -0,0 +1,13 @@
+const getContentType = pathName => {
+  if (pathName.endsWith('.html')) {
+    return 'text/html'
+  } else if (pathName.endsWith('.js')) {
+    return 'application/json'
+  } else if (pathName.endsWith('.css')) {
+    return 'text/css'
+  } else if (pathName.endsWith('.ico')) {
+    return 'image/x-icon'
+  }
+}
+
+module.exports = getContentType
