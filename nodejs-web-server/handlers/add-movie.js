e2c9c346b978b99ceeb4781c61196aa71d2696db
Vilislav Ivanov <vilislav.ivanov91@abv.bg>
2018-07-25 16:39:30 +0300

    add-movie handler parse the recieved data and add it to memory db

--- nodejs-web-server/handlers/add-movie.js
+++ nodejs-web-server/handlers/add-movie.js
@@ -1,4 +1,6 @@
 const fs = require('fs')
+const qs = require('querystring')
+const db = require('../data/database')
 
 const addMovie = (req, res) => {
   if (req.method === 'GET' && req.pathName === '/movie') {
@@ -16,6 +18,21 @@ const addMovie = (req, res) => {
         res.write(data)
         res.end()
       }
+    })
+  } else if (req.method === 'POST' && req.pathName === '/movie') {
+    let body = ''
+
+    req.on('data', data => {
+      body += data
+
+      if (body.length > 1e6) {
+        req.connection.destroy()
+      }
+    })
+    req.on('end', () => {
+      let post = qs.parse(body)
+      db.add(post.title, post.url, post.yearRelease, post.synopsis)
+      res.end()
     })
   } else {
     return true
