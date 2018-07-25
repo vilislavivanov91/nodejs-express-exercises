e4b49e3af3891eb6ab429bce31c0290575bf88cb
Vilislav Ivanov <vilislav.ivanov91@abv.bg>
2018-07-25 21:10:54 +0300

    persistence files mongodb initial commit

---
+++ persistence-files-mongodb/index.js
@@ -0,0 +1,15 @@
+const http = require('http')
+const url = require('url')
+const port = 4242
+const handlers = require('./handlers')
+
+http.createServer((req, res) => {
+  req.pathName = url.parse(req.url).pathname
+  for (const handler of handlers) {
+    if (!handler(req, res)) {
+      break
+    }
+  }
+}).listen(port, () => {
+  console.log(`App up and running at localhost:${port}`)
+})
