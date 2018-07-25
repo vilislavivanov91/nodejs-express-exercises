4637cec709e99f483061eef5c7a96daa23d99ccb
Vilislav Ivanov <vilislav.ivanov91@abv.bg>
2018-07-25 22:46:31 +0300

    Dynamically generate getDetails.html

--- persistence-files-mongodb/handlers/getDetails.js
+++ persistence-files-mongodb/handlers/getDetails.js
@@ -10,6 +10,19 @@ const getDetailsHandler = (req, res) => {
         res.write('page not found')
         res.end()
       } else {
+        const targetedMeme = {
+          memeSrc: 'https://steemitimages.com/DQmYfiDGFmZsgkpq6XPE3WssU6CK9SagBUCHyWVZGmVxU3T/92bd51939ce6e27f773aee3516b2cd6f--happy-monday-its-monday.jpg',
+          title: 'test',
+          description: 'desc',
+          posterSrc: 'https://steemitimages.com/DQmYfiDGFmZsgkpq6XPE3WssU6CK9SagBUCHyWVZGmVxU3T/92bd51939ce6e27f773aee3516b2cd6f--happy-monday-its-monday.jpg'
+        }
+        const result = `<div class="content">
+    <img src="${targetedMeme.memeSrc}" alt=""/>
+    <h3>Title  ${targetedMeme.title}</h3>
+    <p> ${targetedMeme.description}</p>
+    <button><a href="${targetedMeme.posterSrc}">Download Meme</a></button>
+    </div>`
+        data = data.replace('{{replaceMe}}', result)
         res.writeHead(200, {
           'content-type': 'text/html'
         })
