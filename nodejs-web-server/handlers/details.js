65a555122586071bcd6eda6c7446b8a478cbde1b
Vilislav Ivanov <vilislav.ivanov91@abv.bg>
2018-07-25 18:57:23 +0300

    Implemented logic for add movie data from db to details.html

--- nodejs-web-server/handlers/details.js
+++ nodejs-web-server/handlers/details.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const db = require('../data/database')
 const detailsHandler = (req, res) => {
   if (req.method === 'GET' && req.pathName === '/details') {
     fs.readFile('views/details.html', 'utf-8', (err, data) => {
@@ -9,6 +10,17 @@ const detailsHandler = (req, res) => {
         res.write('error')
         res.end()
       } else {
+        const movies = db.get()
+        let result = ''
+        movies.forEach(movie => {
+          result += `<div class="content">
+                      <img src=${movie.url} alt=${movie.title}/>
+                      <h3>Title ${movie.title}</h3>
+                      <h3>Year ${movie.yearRelease}</h3>
+                      <p>${movie.synopsis}</p>
+                    </div>​`
+        })
+        data = data.replace('{{replace}}', result)
         res.writeHead(200, {
           'content-type': 'text/html'
         })
