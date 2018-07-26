31ee02cc85a2138e0ab5441e00df92dd9b142e5b
Vilislav Ivanov <vilislav.ivanov91@abv.bg>
2018-07-26 09:40:32 +0300

    Implemented upload image functionality with formidable

--- persistence-files-mongodb/handlers/addMeme.js
+++ persistence-files-mongodb/handlers/addMeme.js
@@ -1,7 +1,8 @@
 const fs = require('fs')
+const formidable = require('formidable')
 
 const addMemeHandler = (req, res) => {
-  if (req.method === 'GET' && req.pathName === '/meme') {
+  if (req.method === 'GET' && req.pathName === '/addMeme') {
     fs.readFile('./views/addMeme.html', 'utf-8', (err, data) => {
       if (err) {
         res.writeHead(404, {
@@ -15,6 +16,16 @@ const addMemeHandler = (req, res) => {
         })
         res.write(data)
         res.end()
+      }
+    })
+  } else if (req.method === 'POST' && req.pathName === '/addMeme') {
+    const form = new formidable.IncomingForm()
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log(fields)
+        console.log(files)
       }
     })
   } else {
