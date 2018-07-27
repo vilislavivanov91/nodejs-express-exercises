995dd7c1ff21c5bc91bfc649efc1df24d4fc4070
Vilislav Ivanov <vilislav.ivanov91@abv.bg>
2018-07-27 23:34:15 +0300

    filtered search for images with tag equals to selected tag - unfinished

--- persistence-files-mongodb/handlers/filtered.js
+++ persistence-files-mongodb/handlers/filtered.js
@@ -1,4 +1,8 @@
 const fs = require('fs')
+const formidable = require('formidable')
+
+const Image = require('../model/Image')
+const Tag = require('../model/Tag')
 
 const filteredHandler = (req, res) => {
   if (req.method === 'GET' && req.pathName === '/filtered') {
@@ -15,6 +19,34 @@ const filteredHandler = (req, res) => {
         })
         res.write(data)
         res.end()
+      }
+    })
+  } else if (req.method === 'POST' && req.pathName === '/filtered') {
+    const form = new formidable.IncomingForm()
+    form.parse(req, (err, fields) => {
+      if (err) {
+        console.log(err)
+      } else {
+        const tagName = fields.tag
+        const searchedImageIdArr = []
+        Image.find({})
+        .then(images => {
+          images.forEach(image => {
+            const tagIdArr = image.tags
+            tagIdArr.forEach(tagId => {
+              Tag.findById(tagId)
+                .then(tag => {
+                  if (tag.name === tagName) {
+                    searchedImageIdArr.push(image._id)
+                    // Found the looking image
+                  }
+                })
+                .catch(err => console.log(err))
+            })
+          })
+        }).catch(err => {
+          console.log(err)
+        })
       }
     })
   } else {
