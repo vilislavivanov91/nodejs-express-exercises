474a63c9831582e0fd36fdb03c8e1a36b57485f9
Vilislav Ivanov <vilislav.ivanov91@abv.bg>
2018-07-27 19:01:14 +0300

    Now addTag adds tags to db through mongoose

--- persistence-files-mongodb/handlers/addTag.js
+++ persistence-files-mongodb/handlers/addTag.js
@@ -1,4 +1,7 @@
 const fs = require('fs')
+const Tag = require('../model/Tag')
+
+const formidable = require('formidable')
 
 const tagHandler = (req, res) => {
   if (req.method === 'GET' && req.pathName === '/addTag') {
@@ -14,6 +17,26 @@ const tagHandler = (req, res) => {
           'content-type': 'text/html'
         })
         res.write(data)
+        res.end()
+      }
+    })
+  } else if (req.method === 'POST' && req.pathName === '/addTag') {
+    const form = new formidable.IncomingForm()
+    form.parse(req, (err, fields) => {
+      if (err) {
+        console.log(err)
+      } else {
+        const tag = new Tag({
+          name: fields.tagName
+        })
+        tag.tagNameToLower()
+        tag.save(err => {
+          if (err) {
+            console.log(err)
+          } else {
+            console.log('tag saved')
+          }
+        })
         res.end()
       }
     })
