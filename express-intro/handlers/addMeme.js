1b8108e0a3b7292a842622a47c840af4f664eeba
Vilislav Ivanov <vilislav.ivanov91@abv.bg>
2018-07-30 22:21:57 +0300

    added /addMeme and serving addMeme.html

---
+++ express-intro/handlers/addMeme.js
@@ -0,0 +1,22 @@
+const router = require('express').Router()
+
+const genres = [
+  {
+    name: 'RickAndMorty'
+  },
+  {
+    name: 'NotRickAndMorty'
+  }
+]
+const statusHtml = '<div id="errBox"><h2 id="errMsg">Please fill all fields</h2></div>'
+// '<div id="succssesBox"><h2 id="succssesMsg">Movie Added</h2></div>'
+
+router.route('/addMeme')
+  .get((req, res) => {
+    res.render('addMeme', {
+      genres,
+      statusHtml
+    })
+  })
+
+module.exports = router
--- express-intro/handlers/index.js
+++ express-intro/handlers/index.js
@@ -1,8 +1,10 @@
 const homeHander = require('./home')
+const addMemeHandler = require('./addMeme')
 
 const handlers = (app, express) => {
   app.use('/public', express.static('public'))
   app.use(homeHander)
+  app.use(addMemeHandler)
 }
 
 module.exports = handlers
