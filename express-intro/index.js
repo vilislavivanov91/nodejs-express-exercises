edc64eb0ece9d6322a62f2326e8f4e4862264858
Vilislav Ivanov <vilislav.ivanov91@abv.bg>
2018-07-30 18:59:45 +0300

    install and set handlebars

--- express-intro/index.js
+++ express-intro/index.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const app = express()
+const handlebars = require('express-handlebars')
 require('./config/dbConfig')
 require('./handlers')(app)
 
 const port = 2121
 
+app.engine('hbs', handlebars({extname: 'hbs'}))
+app.set('view engine', 'hbs')
+
 app.use(express.static('public'))
+app.get('/t', (req, res) => {
+  res.render('test', {
+    title: 'Hello from hbs'
+  })
+})
 
 app.listen(port, () => {
   console.log(`App up and running at localhost:${port}`)
