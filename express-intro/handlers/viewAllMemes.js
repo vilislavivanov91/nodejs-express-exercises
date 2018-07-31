653c83d27b7d159c65be9dafa055da72724532cd
Vilislav Ivanov <vilislav.ivanov91@abv.bg>
2018-07-31 22:51:54 +0300

    serving all memes from db and sort them by creation date

--- express-intro/handlers/viewAllMemes.js
+++ express-intro/handlers/viewAllMemes.js
@@ -1,8 +1,25 @@
 const router = require('express').Router()
+const Meme = require('../model/Meme')
 
 router.route('/viewAllMemes')
   .get((req, res) => {
-    res.render('viewAllMemes')
+    let allMemesHtml = ''
+    Meme.find({})
+      .sort([['creationDate', -1]])
+      .then(memes => {
+        memes.forEach(meme => {
+          allMemesHtml += `<div class="meme">
+  <a href="/getDetails?id=${meme._id}">
+  <img class="memePoster" src="${meme.filePath}"/>          
+  </div>
+  `
+        })
+        res.render('viewAllMemes', {
+          allMemesHtml
+        })
+      }).catch(() => {
+        res.render('viewAllMemes')
+      })
   })
 
 module.exports = router
