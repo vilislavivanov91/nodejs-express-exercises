4a17779e29cd238472de1b5cccfb90444521346d
Vilislav Ivanov <vilislav.ivanov91@abv.bg>
2018-08-01 21:42:55 +0300

    Added resultHandler - displaying the searched memes

--- express-intro/handlers/index.js
+++ express-intro/handlers/index.js
@@ -4,6 +4,7 @@ const addGenreHandler = require('./addGenre')
 const viewAllMemesHandler = require('./viewAllMemes')
 const memeDetailsHandler = require('./memeDetails')
 const searchMemeHandler = require('./searchMeme')
+const resultHandler = require('./result')
 
 const handlers = (app, express) => {
   app.use('/public', express.static('public'))
@@ -13,6 +14,7 @@ const handlers = (app, express) => {
   app.use(viewAllMemesHandler)
   app.use(memeDetailsHandler)
   app.use(searchMemeHandler)
+  app.use(resultHandler)
 }
 
 module.exports = handlers
---
+++ express-intro/handlers/result.js
@@ -0,0 +1,44 @@
+const router = require('express').Router()
+const Genre = require('../model/Genre')
+const Meme = require('../model/Meme')
+
+router.route('/result')
+  .get((req, res) => {
+    const genreTitle = req.query.genre
+    const searchedString = req.query.name
+
+    Genre.find({
+      title: genreTitle
+    }).then(genres => {
+      const genre = genres[0]
+      const memeArr = genre.memeArr
+      const promiseArr = []
+      const searchedMemes = []
+
+      memeArr.forEach(memeId => {
+        promiseArr.push(
+          Meme.findById(memeId)
+            .then(meme => {
+              const memeTitle = meme.title
+              const memeDesc = meme.description
+
+              if (memeTitle.indexOf(searchedString) !== -1 || memeDesc.indexOf(searchedString) !== -1) {
+                searchedMemes.push(meme)
+              }
+            })
+        )
+      })
+
+      Promise.all(promiseArr).then(() => {
+        console.log('here? - all')
+        console.log(searchedMemes)
+        res.render('result', {
+          resultMemes: searchedMemes
+        })
+      })
+    }).catch(err => {
+      console.log(err)
+    })
+  })
+
+module.exports = router
