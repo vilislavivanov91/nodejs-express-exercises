d7bc3542df01056e52c647843d0e7cc52b1e12fa
Vilislav Ivanov <vilislav.ivanov91@abv.bg>
2018-08-02 14:36:18 +0300

    added viewAll handler and display all books from db

--- book-library/hanlders/index.js
+++ book-library/hanlders/index.js
@@ -1,7 +1,9 @@
 const homeHandler = require('./home')
 const addBookHandler = require('./addBook')
+const viewAllHandler = require('./viewAll')
 
 module.exports = (app) => {
   app.use(homeHandler)
   app.use(addBookHandler)
+  app.use(viewAllHandler)
 }
---
+++ book-library/hanlders/viewAll.js
@@ -0,0 +1,13 @@
+const router = require('express').Router()
+const Book = require('../model/Book')
+
+router.route('/viewAll').get((req, res) => {
+  Book.find({}).sort('yearRelease').then(books => {
+    console.log(books)
+    res.render('viewAll', {
+      books
+    })
+  })
+})
+
+module.exports = router
