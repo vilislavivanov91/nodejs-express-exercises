89fdeabf5bd5059ffe75c983eed261360ac09f7e
Vilislav Ivanov <vilislav.ivanov91@abv.bg>
2018-08-02 11:46:02 +0300

    Added default layout in handlebars and main.hbs

--- book-library/config/appConfig.js
+++ book-library/config/appConfig.js
@@ -2,7 +2,7 @@ const handlerbars = require('express-handlebars')
 const port = 4242
 
 module.exports = (app, express) => {
-  app.engine('hbs', handlerbars({extname: 'hbs'}))
+  app.engine('hbs', handlerbars({extname: 'hbs', defaultLayout: 'main'}))
   app.set('view engine', 'hbs')
 
   app.use(express.static('public'))
