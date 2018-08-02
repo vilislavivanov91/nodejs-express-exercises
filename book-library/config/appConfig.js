f6c2fe935d2fdbe2ceb292f6dfcbca8869fd049c
Vilislav Ivanov <vilislav.ivanov91@abv.bg>
2018-08-02 12:51:52 +0300

    added  and set body-parser

--- book-library/config/appConfig.js
+++ book-library/config/appConfig.js
@@ -1,9 +1,12 @@
 const handlerbars = require('express-handlebars')
+const bodyParser = require('body-parser')
 const port = 4242
 
 module.exports = (app, express) => {
   app.engine('hbs', handlerbars({extname: 'hbs', defaultLayout: 'main'}))
   app.set('view engine', 'hbs')
+
+  app.use(bodyParser.urlencoded({extended: true}))
 
   app.use(express.static('public'))
 
