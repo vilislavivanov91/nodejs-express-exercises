d8a14ef79244fb9af489d868b628db6b74b7796c
Vilislav Ivanov <vilislav.ivanov91@abv.bg>
2018-08-02 12:58:48 +0300

    addBook handler and saves the received date to DB

--- book-library/hanlders/addBook.js
+++ book-library/hanlders/addBook.js
@@ -1,8 +1,40 @@
 const router = require('express').Router()
+const Book = require('../model/Book')
 
-router.route('/addBook').get((req, res) => {
+router.route('/addBook')
+.get((req, res) => {
   res.render('addBook', {
     title: 'Hello from add book handler'
+  })
+}).post((req, res) => {
+  const title = req.body.title
+  const url = req.body.url
+  const yearRelease = req.body.yearRelease
+  const author = req.body.author
+
+  const book = new Book({
+    title,
+    url,
+    yearRelease,
+    author
+  })
+
+  book.save(err => {
+    if (err) {
+      const errMessageArr = []
+      const errors = err.errors
+      for (const key in errors) {
+        if (errors.hasOwnProperty(key)) {
+          const currErr = errors[key]
+          errMessageArr.push(currErr)
+        }
+      }
+      res.render('addBook', {
+        errMessageArr
+      })
+    } else {
+      res.redirect('/viewAll')
+    }
   })
 })
 
